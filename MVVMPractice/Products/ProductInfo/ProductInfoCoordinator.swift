ee8fdf5161aad15410f7c5c7f4da132bf67d522f
Ravshanbek Tursunbaev <rtursunbaev9822@gmail.com>
2023-03-28 14:19:05 +0500

    Updating Product Info Coordinator

--- MVVMPractice/Products/ProductInfo/ProductInfoCoordinator.swift
+++ MVVMPractice/Products/ProductInfo/ProductInfoCoordinator.swift
@@ -5,12 +5,18 @@
 //  Created by Ravshanbek Tursunbaev on 2023/03/28.
 //
 
-import Foundation
+import UIKit
 
 class ProductInfoCoordinator: Coordinator{
-    var presenter: UINavigationController
     
     var childCoordinators: [Coordinator]
+    var presenter: UINavigationController
+    private let viewModel: ProductInfoViewModel
+    
+    init(presenter: UINavigationController) {
+        self.presenter = presenter
+        viewModel = ProductInfoViewModel()
+    }
     
     func start() {
         <#code#>
